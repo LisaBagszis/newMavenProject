248cc62e6150b0306277fdc10b3a78246ba65b65
LisaBagszis <lisa.bagszis@gmail.com>
2023-03-02 10:52:53 +0100

    Idea gelöscht

---
+++ src/test/BonusAufgabeTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusAufgabeTest {
+
+    @Test
+    void ifTheFirstNumberisBiggerThanTheSecond() {
+        //Given
+        int zahl1 = 5;
+        int zahl2 = 4;
+        boolean subNumAfromNumB = true;
+
+        //When - wichtige Info: das erste unterhalb steht für die Klasse / Datei auf die sich das Ganze bezieht und das zweite Wort auf die Methode darin
+        int actual = BonusAufgabe.sumSquareOrSubstract(zahl1, zahl2, subNumAfromNumB);
+
+        //Then
+        assertEquals(-1 , actual);
+    }
+
+    @Test
+    void ifTheFirstNumberIsSmallerThanTheSecond() {
+
+        //Given
+        int zahl1 = 5;
+        int zahl2 = 6;
+
+        //When
+        int actual = BonusAufgabe.sumSquareOrSubstract(zahl1, zahl2);
+
+        //Then
+        assertEquals (30, actual);
+
+    }
+
+    @Test
+    void ifTheNumbersAreTheSame() {
+
+        //Given
+        int zahl1 = 6;
+        int zahl2 = 6;
+
+        //When
+        int actual = BonusAufgabe.sumSquareOrSubstract(zahl1, zahl2);
+
+        //Then
+        assertEquals (36, actual);
+    }
+
+}
